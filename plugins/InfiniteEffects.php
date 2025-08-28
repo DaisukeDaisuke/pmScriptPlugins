5ed0ab359c7e87d7230cdbc23b32f5fb8329be01
DaisukeDaisuke <17798680+DaisukeDaisuke@users.noreply.github.com>
2025-08-28 10:12:03 +0900

    a

---
+++ plugins/InfiniteEffects.php
@@ -0,0 +1,46 @@
+<?php
+
+
+declare(strict_types=1);
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\server\DataPacketSendEvent;
+use pocketmine\network\mcpe\protocol\MobEffectPacket;
+
+/**
+ * @name InfiniteEffects
+ * @api 5.30.0
+ * @description Implement an infinite effect. Just use \pocketmine\utils\Limits::INT32_MAX as the effect duration!
+ * @version 1.0.0
+ * @main InfiniteEffects_MpqCz
+ * @author DaisukeDaisuke
+ */
+class InfiniteEffects_MpqCz extends PluginBase implements Listener{
+	protected function onEnable() : void{
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+
+	public function Data(DataPacketSendEvent $event) : void{
+		$packets = $event->getPackets();
+		$changed = false;
+		foreach($packets as $key => $packet){
+			if($packet instanceof MobEffectPacket && $packet->duration >= 630720000){//1 year
+				$packet = MobEffectPacket::create(
+					actorRuntimeId: $packet->actorRuntimeId,
+					eventId: $packet->eventId,
+					effectId: $packet->effectId,
+					amplifier: $packet->amplifier,
+					particles: $packet->particles,
+					duration: -1,
+					tick: $packet->tick,
+				);
+				$changed = true;
+				$packets[$key] = $packet;
+			}
+		}
+		if($changed){
+			$event->setPackets($packets);
+		}
+	}
+}
